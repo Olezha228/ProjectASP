8c60696e1defcbd39ef51367f7c6b67c8448d58d
oleg <olegfide@mail,ru>
2023-02-18 23:46:29 +0500

    created html files which are open when requested

---
+++ Math.UnitTests/CalculatorTests.cs
@@ -0,0 +1,7 @@
+﻿namespace Math.UnitTests
+{
+    public class CalculatorTests
+    {
+
+    }
+}
---
+++ Math/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Math
+{
+    public class Calculator
+    {
+        public static int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
