ab3597e82a45335be95bc9ff4f56229c2b6e7092
oleg <olegfide@mail,ru>
2023-02-20 12:20:45 +0500

    changed tests

--- Math.UnitTests/CalculatorTests.cs
+++ Math.UnitTests/CalculatorTests.cs
@@ -8,11 +8,11 @@ namespace Math.UnitTests
         [Test]
         public void Add_WhenPositive_ValidExpected()
         {
-            var first = 5;
+            var first = 6;
             var second = 10;
 
             var result = Calculator.Add(first, second);
-            var expected = 15;
+            var expected = 16;
 
             Assert.AreEqual(expected, result);
         }
